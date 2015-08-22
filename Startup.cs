c9b9c362d4a63c625bff2c019e4fea2d02415792
Saunders1732 <RSaunders1732@yahoo.com>
2015-08-22 15:25:53 +0000

    cleanup post swashbuckle attempt

--- Startup.cs
+++ Startup.cs
@@ -26,8 +26,7 @@ namespace TCub
             // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
             // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
             // services.AddWebApiConventions();
-
-           // services.AddSingleton(_ => Configuration);
+            
         }
 
         // Configure is called after ConfigureServices is called.
@@ -45,6 +44,7 @@ namespace TCub
             app.UseMvc();
             // Add the following route for porting Web API 2 controllers.
             // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            
         }
     }
 }
